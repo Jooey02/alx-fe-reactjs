afecb089734d19d814a977103af784174cd53f3e
Jooey Otu <140718796+Jooey02@users.noreply.github.com>
2024-08-18 23:40:41 +0100

    correced

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,6 +1,6 @@
 // src/UserDetails.jsx
 import React from 'react';
-import { useUser } from '../UserContext';
+import { useUser } from './UserContext';
 
 function UserDetails() {
   const userData = useUser();
