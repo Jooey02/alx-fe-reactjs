5ad01523ab193af497a178b10313f73db9124df8
Jooey Otu <140718796+Jooey02@users.noreply.github.com>
2024-08-18 22:26:41 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const MainContent = () => {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-      <h2>Main Content Area</h2>
-      <p>Welcome to the main content area. This section can be styled further.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 };
