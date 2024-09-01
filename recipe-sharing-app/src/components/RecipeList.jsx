83c3c5270e4df49553ed8569e77ba8144f879b45
Jooey Otu <140718796+Jooey02@users.noreply.github.com>
2024-09-01 12:31:47 +0100

    Created a recipie app using zustand store

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+// src/components/RecipeList.jsx
+import React from 'react'
+import useRecipeStore from '../store/recipeStore'
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes)
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default RecipeList
