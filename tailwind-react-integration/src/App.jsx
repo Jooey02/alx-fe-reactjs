995d6e30c21d80e7f1510c8e6b477ae3ea9cd447
Jooey Otu <140718796+Jooey02@users.noreply.github.com>
2024-09-15 20:12:51 +0100

    Added tailwind css

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-4 bg-blue-100">
+      <h1 className="text-2xl font-bold text-blue-600">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  )
+}
+
+export default App
